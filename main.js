7fbc1378866005a971189f54eed2e218d9ab2938
ritviksakpal1 <149887698+ritviksakpal1@users.noreply.github.com>
2023-12-14 20:33:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ let randomNums1 = Math.floor(Math.random() * 6 + 1)
 let randomNums2 = Math.floor(Math.random() * 6 + 1)
 var myImg1 = document.getElementById("img1")
 var myImg2 = document.getElementById("img2")
-let imgName1 = "/images/dice" + randomNums1 + ".png"
-let imgName2 = "/images/dice" + randomNums2 + ".png"
+let imgName1 = "images/dice" + randomNums1 + ".png"
+let imgName2 = "images/dice" + randomNums2 + ".png"
 myImg1.setAttribute('src', imgName1)
 myImg2.setAttribute('src', imgName2)
 
